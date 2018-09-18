a16c0fb5d3010239de20a76913f8f407bab09136
yaseenkola <yaseenkola@gmail.com>
2018-09-18 10:04:46 +0000

    Create login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  
+  def new
+  # will simply render the form
+  end
+
+  def create
+  # will create the session and move the user to logged in state
+  end
+
+  def destroy
+  # will end the session or simulate logged out state
+  end
+
+end 
