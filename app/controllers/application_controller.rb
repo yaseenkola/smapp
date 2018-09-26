c6db0cefb679b3cde4d0fc1676bcce6cc08152f6
yaseenkola <yaseenkola@gmail.com>
2018-09-26 18:44:32 +0000

    update search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,21 @@ class ApplicationController < ActionController::Base
   add_flash_types :danger,:success
   
  
-  helper_method :current_user, :logged_in?
+  # helper_method :current_user, :logged_in?
 
-  def current_user
-   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
-  def logged_in?
-   !!current_user
-  end
+  # def logged_in?
+  #  !!current_user
+  # end
 
-  def require_user
-   if !logged_in?
-    flash[:danger] = "You must be logged in to perform that action"
-    redirect_to root_path
-   end
-  end
+  # def require_user
+  #  if !logged_in?
+  #   flash[:danger] = "You must be logged in to perform that action"
+  #   redirect_to root_path
+  #  end
+  # end
   
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
-  # def home
-  #   redirect_to admissions_path if logged_in?
-  # end
+  def home
+    redirect_to admissions_path if logged_in?
+  end
   
 end
