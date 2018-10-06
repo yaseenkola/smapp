6ba079506ae1a9fc81ca89e953e11c02c1119418
yaseenkola <yaseenkola@gmail.com>
2018-10-06 10:08:21 +0000

    Update migration

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,7 +8,9 @@ if Rails.env.production?
   
   :aws_access_key_id => ENV['S3_ACCESS_KEY'],
   
-  :aws_secret_access_key => ENV['S3_SECRET_KEY']
+  :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+  
+  :region =>  'us-east-2'
   
   }
   
